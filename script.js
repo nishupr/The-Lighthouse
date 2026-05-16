7e090b25688fffcef24786171cb0ff4d4a774c3a
Ras0105 <smt29nov@gmail.com>
2026-05-16 13:22:29 +0530

    fixing issue #30 by improving the css of the back to top button

--- script.js
+++ script.js
@@ -277,6 +277,16 @@ window.addEventListener("scroll", () => {
   }
 });
 
+// Show/hide on scroll
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 300) {
+    backToTopBtn.classList.add("visible");
+  } else {
+    backToTopBtn.classList.remove("visible");
+  }
+});
+
+// Scroll to top on click
 backToTopBtn.addEventListener("click", () => {
   window.scrollTo({
     top: 0,
